9dfec0236be25c08806a4c83936bad943b52417a
sayalex1208-jpg <sayalex1208@gmail.com>
2026-07-01 16:43:53 +0700

    fix case 2 display all student grades

--- member6.c
+++ member6.c
@@ -2,15 +2,28 @@
 #include <string.h>
 
 #define MAX_STUDENTS 100
+#define MAX_SUBS 10
 
-typedef struct
-{
-    int id;
-    char name[50];
-    float score;
-} student;
+struct Student{
+    int id[10];
+    char name[30];
+    float marks[MAX_SUBS];
+    float total;
+    float average;
+    char grade;
+    char userName;
+    
+};
 
-void saveToFile(student s[], int count)
+
+// global variables
+int n;
+int num_sub;
+
+
+
+struct Student students[MAX_STUDENTS];
+void saveToFile(struct Student students[], int count)
 {
     FILE *fp = fopen("students.txt", "wb"); // "wb" = overwrite each save
     if (fp == NULL)
@@ -18,12 +31,12 @@ void saveToFile(student s[], int count)
         printf("Error opening file.\n");
         return;
     }
-    fwrite(s, sizeof(student), count, fp);
+    fwrite(students, sizeof(students), count, fp);
     fclose(fp);
     printf("Saved %d student(s) to file.\n", count);
 }
 
-int loadFromFile(student s[], int maxSize)
+int loadFromFile(struct Student students[], int maxSize)
 {
     FILE *fp = fopen("students.txt", "rb"); // "rb" = read binary
     if (fp == NULL)
@@ -31,15 +44,21 @@ int loadFromFile(student s[], int maxSize)
         printf("No saved file found.\n");
         return 0;
     }
-    int count = fread(s, sizeof(student), maxSize, fp);
+    int count = fread(students, sizeof(students), maxSize, fp);
     fclose(fp);
     printf("Loaded %d student(s) from file.\n", count);
     return count;
 }
 
+
+//function prototype
+void add_student(struct Student students[]);
+char getgrade(float average);
+void Individual();
+
 int main()
 {
-    student list[MAX_STUDENTS];
+    struct Student list[MAX_STUDENTS];
     int count = 0;
     int choice;
 
@@ -48,7 +67,7 @@ int main()
         printf("\n--- Student Management System ---\n");
         printf("1. Add Student Data\n");
         printf("2. Grade Calculation\n");
-        printf("3. Search Student ID\n");
+        printf("3. Individual Result\n");
         printf("4. Show Student score (highest, lowest, average)\n");
         printf("6. Save to File\n");
         printf("7. Load from File\n");
@@ -58,14 +77,38 @@ int main()
 
         switch (choice)
         {
-        case 1:
-            // TODO: hook up to teammate's "add student" function
+            case 1:
+            add_student(students);
             break;
-        case 2:
-            // TODO: hook up to teammate's "grade calculation" function
+            case 2:
+            for (int i = 0; i < MAX_STUDENTS; i++) {
+            printf("%-5d %-20s %-10.2f %c\n",
+            students[i].id,
+            students[i].name,
+            students[i].average,
+            getgrade(students[i].average));
+            }
+            break;
+        //     int target;
+        //     int index = -1;
+        //     printf("Please enter student id to check the grade: ");
+        //     scanf("%d", &target);
+
+        //     for(int i =0; i < MAX_STUDENTS; i++ ){
+        //     if(target == students[i].id){
+        //         index = i;
+        //         break;
+        //     }
+        //     }
+        //     if (index == -1) {
+        // printf("Student not found.\n");
+        //  } else {
+        // printf("Grade: %c\n", getgrade(students[index].average));
+        // }
             break;
         case 3:
-            // TODO: hook up to teammate's "search by ID" function
+        
+            Individual();
             break;
         case 4:
             // TODO: hook up to teammate's "show stats" function
